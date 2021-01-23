33045516316d94ee4d0641485e805925cde41628
Floupy <tmurnieks@gmail.com>
2021-01-23 12:27:04 +0200

    12:26

---
+++ BankingApplication/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingApplication
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; }
+        public decimal Balance {
+            get
+            {
+                decimal balance = 0;
+                foreach(var t in transactions)
+                {
+                    balance += t.Amount;
+                }
+                return balance;
+            }
+        }
+        public string Currency { get; }
+        public DateTime DateCreated { get; }
+
+        private List<Transaction> transactions = new List<Transaction>();
+
+        public BankAccount(string number, string owner, decimal balance, string currency)
+        {
+            this.Number = number;
+            this.Owner = owner;
+            this.Currency = currency;
+            this.DateCreated = DateTime.Now;
+
+            this.AddToBalance(balance, DateTime.Now, "Initial balance");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"- account {this.Number} created on {this.DateCreated}  belongs to {this.Owner} and has a balance of {this.Balance} {this.Currency} ");
+        }
+
+
+        public void AddToBalance(decimal amount, DateTime date, string note)
+        {
+            transactions.Add(new Transaction(amount, date, note));
+        }
+
+        
+
+    }
+}
--- BankingApplication/Program.cs
+++ BankingApplication/Program.cs
@@ -18,38 +18,20 @@ namespace BankingApplication
             accounts.Add(new BankAccount("DDB123456790", "Anna Kurnikova", 10000000, "RUB"));
             accounts.Add(new BankAccount("DDB123456791", "Jenifer Lopez", 200, "USD"));
 
-            foreach(var account in accounts)
+            accounts[0].AddToBalance(1000000, new DateTime(2015, 12, 31, 5, 10, 20), "Donations");
+            accounts[0].AddToBalance(2000, DateTime.Now, "Lecture");
+
+
+            foreach (var account in accounts)
             {
                 account.Print();
             }
 
             
-
+            
         }
     }
 
-    public class BankAccount
-    {
-        public string Number { get;  }
-        public string Owner { get;  }
-        public decimal Balance { get;  }
-        public string Currency { get; }
-        public DateTime DateCreated { get; }
-
-        public BankAccount(string number, string owner, decimal balance, string currency)
-        {
-            this.Number = number;
-            this.Owner = owner;
-            this.Balance = balance;
-            this.Currency = currency;
-            this.DateCreated = DateTime.Now;
-        }
-
-        public void Print()
-        {
-            Console.WriteLine($"- account {this.Number} created on {this.DateCreated}  belongs to {this.Owner} and has a balance of {this.Balance} {this.Currency} ");
-        }
-
-    }
+    
     
 }
